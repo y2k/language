864c1560f194b96629ec984a33c486a5354acb6b
y2k <itwisterlx@gmail.com>
2020-07-14 11:23:07 +0300

    add constructors

--- extractor/src/extractor/core.clj
+++ extractor/src/extractor/core.clj
@@ -10,19 +10,27 @@
 (defn getParams [method]
   (->>
    (.getParameterTypes method)
-   (map #(do {:type (.getCanonicalName %1)}))))
+   (map #(.getCanonicalName %1))))
+
+(defn getConstructors [class]
+  (->>
+   (.getConstructors class)
+   (map #(getParams %1))))
 
 (defn getMethods [class]
   (->>
    (.getMethods class)
    (map #(do {:name (.getName %1)
               :is-static (java.lang.reflect.Modifier/isStatic (.getModifiers %1))
-              :return (.getCanonicalName (.getReturnType %1)) :params (getParams %1)}))))
+              :return (.getCanonicalName (.getReturnType %1))
+              :params (getParams %1)}))))
 
 (defn handleEntity [classLoader entry]
   (let [name (filenameToClsName (.getName entry))
         class (.loadClass classLoader name)]
-    {:name name :methods (getMethods class)}))
+    {:name name
+     :constructors (getConstructors class)
+     :methods (getMethods class)}))
 
 (defn extractAllClasses [jarFile outFile]
   (let [jar (java.util.jar.JarFile. jarFile)
@@ -32,10 +40,17 @@
      (enumeration-seq)
      (filter #(and (.endsWith (.getName %1) ".class") (not= (.getName %1) "java/lang/Object.class")))
      (filter #(= "android.widget.Toast" (filenameToClsName (.getName %1))))
+     (take 50)
      (map #(handleEntity classLoader %1))
      (saveObjectToFile outFile))))
 
 (comment
+
+  (->>
+   (.getClass "")
+   (.getConstructors)
+   (map #(do {:params (getParams %1)})))
+
   (let [home (java.lang.System/getenv "HOME")
         jarFile (java.io.File. (str home "/Projects/language/extractor/__data/android-4.1.1.4.jar"))
         outFile (str home "/out.json")]
