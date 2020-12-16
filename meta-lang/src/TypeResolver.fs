b91266c98e2115f97180d4955ec6764b96cff6cf
y2k <itwisterlx@gmail.com>
2020-12-16 23:36:01 +0300

    add context with functions

--- meta-lang/src/TypeResolver.fs
+++ meta-lang/src/TypeResolver.fs
@@ -4,20 +4,38 @@ open MetaLang
 module E = ExternalTypeResolver
 
 type ResolvedInfo = Map<string, Type>
+type Context = { functions: Map<string, Type list> }
 
-let rec private resolve' (ext: E.t) program: Node * ResolvedInfo =
+let rec private resolve' (ext: E.t) (ctx: Context) program: Node * ResolvedInfo =
     let resolve = resolve' ext
 
     match program with
     | Module (imports, nodes) ->
-        let (nodes, ris) = nodes |> List.map resolve |> List.unzip
-        Module (imports, nodes), ris |> List.last
+        let (_, nodes) =
+            nodes
+            |> List.fold
+                (fun (ctx, nodes) node ->
+                    let (node', _) = resolve ctx node
+
+                    let ctx' =
+                        { ctx with
+                              functions =
+                                  match node' with
+                                  | Defn (name, ps, _) ->
+                                      let types = ps |> List.map snd
+                                      Map.add name types ctx.functions
+                                  | _ -> ctx.functions }
+
+                    ctx', nodes @ [ node' ])
+                ({ functions = Map.empty }, [])
+
+        Module(imports, nodes), Map.empty
     | Defn (name, ps, body) ->
         let ri =
             body
             |> List.fold
                 (fun a node ->
-                    let (_, ri) = resolve node
+                    let (_, ri) = resolve ctx node
                     ri |> Map.fold (fun a1 k v -> Map.add k v a1) a)
                 Map.empty
 
@@ -42,6 +60,22 @@ let rec private resolve' (ext: E.t) program: Node * ResolvedInfo =
                 Map.empty
 
         program, ri
+    | Call (callName, args) ->
+        let funcSign = Map.find callName ctx.functions
+
+        let ri =
+            args
+            |> List.mapi (fun i n -> n, funcSign.[i])
+            |> List.fold
+                (fun a (x, type') ->
+                    match x with
+                    | Symbol name -> Map.add name type' a
+                    | _ -> a)
+                Map.empty
+
+        program, ri
     | other -> other, Map.empty
 
-let resolve ext program = resolve' ext program |> fst
+let resolve ext program =
+    resolve' ext { functions = Map.empty } program
+    |> fst
