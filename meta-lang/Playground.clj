e58aa7cbb534932f1ea823d9c255ea2c5af8cba2
y2k <itwisterlx@gmail.com>
2021-08-02 18:57:36 +0400

    configure tasks

--- meta-lang/Playground.clj
+++ meta-lang/Playground.clj
@@ -1,19 +1,18 @@
 (module
-
  (def state {})
 
  ;; ('a -> 'a) -> 'a
  (defn dispatch [f]
-  (sys/intrinsic_set state model (f (:model state))))
+   (sys/intrinsic_set state model (f (:model state))))
 
   ;; () -> 'a
  (defn resolveEnv [])
 
   ;; Dictionary -> 'a
  (defn textView [props]
-  (let [ctx (:ctx (resolveEnv))
-    tv (sys/intrinsic_new android.widget.TextView ctx)]
-    (sys/intrinsic_invoke android.widget.TextView/setText tv (:text props))))
+   (let [ctx (:ctx (resolveEnv))
+         tv (sys/intrinsic_new android.widget.TextView ctx)]
+     (sys/intrinsic_invoke android.widget.TextView/setText tv (:text props))))
 
   ;; int -> 'a
  (defn intToString [value]
@@ -25,34 +24,34 @@
 
   ;; () -> 'a
  (defn onClick []
-  (dispatch onClickHandle))
+   (dispatch onClickHandle))
 
   ;; Dictionary -> 'a
  (defn view [model]
-  (textView {:onClick onClick
-    :text (intToString (:count model))})))
+   (textView {:onClick onClick
+              :text (intToString (:count model))})))
 
 (module
-  (def state {})
+ (def state {})
   ;; ('a -> 'b) -> 'c
-  (defn dispatch [f]
-    (sys/intrinsic_set state model (f (:model state))))
+ (defn dispatch [f]
+   (sys/intrinsic_set state model (f (:model state))))
   ;; Unit -> 'a
-  (defn resolveEnv [] ???)
+ (defn resolveEnv [] ???)
   ;; Dictionary -> 'a
-  (defn textView [props]
-    (let [ctx (:ctx (resolveEnv ))
-          tv (sys/intrinsic_new android.widget.TextView ctx)]
-      (sys/intrinsic_invoke android.widget.TextView/setText tv (:text props))))
+ (defn textView [props]
+   (let [ctx (:ctx (resolveEnv))
+         tv (sys/intrinsic_new android.widget.TextView ctx)]
+     (sys/intrinsic_invoke android.widget.TextView/setText tv (:text props))))
   ;; int -> 'a
-  (defn intToString [value]
-    (sys/intrinsic_invoke_static java.lang.String/valueOf value))
+ (defn intToString [value]
+   (sys/intrinsic_invoke_static java.lang.String/valueOf value))
   ;; Dictionary -> 'a
-  (defn onClickHandle [model]
-    {:count (add 1 (:count model))})
+ (defn onClickHandle [model]
+   {:count (add 1 (:count model))})
   ;; Unit -> 'a
-  (defn onClick []
-    (dispatch onClickHandle))
+ (defn onClick []
+   (dispatch onClickHandle))
   ;; Dictionary -> 'a
-  (defn view [model]
-    (textView {:onClick onClick :text (intToString (:count model))})))
+ (defn view [model]
+   (textView {:onClick onClick :text (intToString (:count model))})))
