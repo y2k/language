b54ad17b1f0897dcf64d6a32fb3ebe006478675a
y2k <itwisterlx@gmail.com>
2021-08-24 22:46:46 +0400

    add sexp parser

--- meta-lang/src/LanguageParser.fs
+++ meta-lang/src/LanguageParser.fs
@@ -1,13 +1,30 @@
 module LanguageParser
 
+type ExtLang =
+    | ExtModule of ExtLang list
+    | ExtDefn of string
+
+type sexp =
+    | Atom of string
+    | List of sexp list
+
 open FParsec
 
-let lbracket: Parser<string, unit> = pstring "("
-let rbracket = pstring ")"
+let private lbracket: Parser<_, unit> = choice [ pstring "("; pstring "[" ]
+let private rbracket = choice [ pstring ")"; pstring "]" ]
 
-let def _ = pstring "def"
-let a _ = pipe2
-let defn _ = pstring "defn"
+let private patom =
+    (many1Satisfy (fun ch -> isLetter ch || isDigit ch)
+     |>> (fun name -> Atom name))
 
-let module' =
-    lbracket >>. pstring "module" >>. rbracket
+let psexp =
+    let expr, exprImpl = createParserForwardedToRef ()
+
+    exprImpl
+    := between
+        (spaces .>> lbracket)
+        rbracket
+        ((many (choice [ patom; expr ] .>> spaces))
+         |>> List)
+
+    expr
