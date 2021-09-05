23fe1f122de58f8dd8b9b13eb113611166b9b747
y2k <itwisterlx@gmail.com>
2021-09-05 12:21:04 +0400

    add tests

--- meta-lang/src/Interpreter.fs
+++ meta-lang/src/Interpreter.fs
@@ -2,4 +2,63 @@ module Interpreter
 
 open MetaLang
 
-let run (_: string) (_: obj list) (_: Node) : obj = failwith "not implemented"
+let private findFunction funcs funcName funArgs =
+    funcs
+    |> List.tryPick
+        (function
+        | Defn (n, argTypes, retType, body) when
+            n = funcName
+            && (List.length argTypes = List.length funArgs)
+            ->
+            Some(argTypes, retType, body)
+        | _ -> None)
+    |> Option.defaultWith (fun _ -> failwithf "Can't find function '%s'" funcName)
+
+type Context =
+    private
+        { funcs: Node list
+          funArgs: obj list
+          argTypes: (string * Type) list }
+
+let findValueInContext (ctx: Context) symName : obj =
+    let argIndex =
+        ctx.argTypes
+        |> List.tryFindIndex (fun (n, _) -> n = symName)
+        |> Option.defaultWith (fun _ -> failwithf "Cant find value for %O" symName)
+
+    ctx.funArgs.[argIndex]
+
+let rec invokeNode (ctx: Context) (body: Node) : obj =
+    match body with
+    | Symbol symName -> findValueInContext ctx symName
+    | Call (callFunName, callArgs) ->
+        let (fargTypes, _, fbody) =
+            findFunction ctx.funcs callFunName callArgs
+
+        let ctx2 =
+            { ctx with
+                  argTypes = fargTypes
+                  funArgs =
+                      callArgs
+                      |> List.map (fun argBody -> invokeNode ctx argBody) }
+
+        fbody
+        |> List.map (fun b -> invokeNode ctx2 b)
+        |> List.last
+    | _ -> failwith "not implemented"
+
+let run (funcName: string) (funArgs: obj list) (program: Node) : obj =
+    match program with
+    | Module (_, funcs) ->
+        let (argTypes, _, body) = findFunction funcs funcName funArgs
+
+        let ctx =
+            { funcs = funcs
+              funArgs = funArgs
+              argTypes = argTypes }
+
+        body
+        |> List.map (fun b -> invokeNode ctx b)
+        |> List.last
+
+    | n -> failwithf "Unsupported program root node (%O)" n
--- meta-lang/test/InterpreterTests.fs
+++ meta-lang/test/InterpreterTests.fs
@@ -1,5 +1,43 @@
 module InterpreterTests
 
+open Swensen.Unquote
+
+[<Xunit.Fact>]
+let test4 () =
+    """
+    (module (defn main [a b] (+ a b)))
+    """
+    |> LanguageParser.compile
+    |> MetaLang.mapToCoreLang
+    |> Interpreter.run "main" [ box 1; box 2 ]
+    |> fun actual ->
+        let actual = unbox actual
+        test <@ 3 = actual @>
+
+[<Xunit.Fact>]
+let test3 () =
+    """
+    (module (defn foo [c d] c) (defn main [a b] (foo a b)))
+    """
+    |> LanguageParser.compile
+    |> MetaLang.mapToCoreLang
+    |> Interpreter.run "main" [ box 1; box 2 ]
+    |> fun actual ->
+        let actual = unbox actual
+        test <@ 1 = actual @>
+
+[<Xunit.Fact>]
+let test2 () =
+    """
+    (module (defn main [a b] b))
+    """
+    |> LanguageParser.compile
+    |> MetaLang.mapToCoreLang
+    |> Interpreter.run "main" [ box 1; box 2 ]
+    |> fun actual ->
+        let actual = unbox actual
+        test <@ 2 = actual @>
+
 [<Xunit.Fact>]
 let test () =
     """
@@ -8,6 +46,6 @@ let test () =
     |> LanguageParser.compile
     |> MetaLang.mapToCoreLang
     |> Interpreter.run "main" [ box 1; box 2 ]
-    |> ignore
-
-()
+    |> fun actual ->
+        let actual = unbox actual
+        test <@ 1 = actual @>
