eb565426b5aa9c1852e2d1474d07d6e5b3ce09ce
y2k <itwisterlx@gmail.com>
2021-09-29 13:45:14 +0400

    add bot sample

--- meta-lang/Playground.CatBot.clj
+++ meta-lang/Playground.CatBot.clj
@@ -1,10 +1,16 @@
 (module
 
+ (defn main2 [env]
+   (->
+    (get-in env [:env :token])
+    (sprintf "https://api.giphy.com/v1/gifs/random?rating=pg&api_key=%s&tag=cat")
+    (download-json)
+    (get-in [:data :image_mp4_url])))
+
  (defn on-loaded [json]
-   (let [url (:image_mp4_url (:data json))]
-     [:send-messge {:text url}]))
+   (get-in [:data :image_mp4_url]))
 
  (defn main [env]
-   (let [token (:token (:env env))
+   (let [token (get-in env [:env :token])
          url (str "https://api.giphy.com/v1/gifs/random?rating=pg&api_key=" token "&tag=cat")]
      [:download-json {:url url :on-complete on-loaded}])))
