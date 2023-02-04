b6998294befa79e121a6a017d8932dfc1cc4593a
y2k <itwisterlx@gmail.com>
2023-02-04 16:35:49 +0300

    fix tests

--- meta-lang/test/MacroExpandTests.fs
+++ meta-lang/test/MacroExpandTests.fs
@@ -7,21 +7,29 @@ open MetaLang
 let private validate actual expected =
     let actual =
         actual
+        |> sprintf "(module (defn foo [] %s))"
         |> LanguageParser.parse
         |> MacroExpand.expandSexp
         |> LanguageParser.compileToExtNode
         |> mapToCoreLang
         |> MacroExpand.run
 
-    let expected = expected |> LanguageParser.compile |> mapToCoreLang
+    let expected =
+        expected
+        |> sprintf "(module (defn foo [] %s))"
+        |> LanguageParser.compile
+        |> mapToCoreLang
+
     test <@ expected = actual @>
+
+// [<Fact>]
+// let test3 () =
+//     validate "(->> a (b) (c d))" " (c d (b a))"
 
 [<Fact>]
 let test2 () =
-    validate "(module (defn foo [] (->> a (b) (c d))))" "(module (defn foo [] (c d (b a))))"
+    validate "(->> a (b) (c d))" " (c d (b a))"
 
 [<Fact>]
 let test () =
-    validate
-        "(module (defn foo [] (case x a r_a b r_b r_def)))"
-        "(module (defn foo [] (let [__var__ x] (if (= __var__ a) r_a (if (= __var__ b) r_b r_def)) ) ))"
+    validate "(case x a r_a b r_b r_def)" "(let [__var__ x] (if (= __var__ a) r_a (if (= __var__ b) r_b r_def)))"
