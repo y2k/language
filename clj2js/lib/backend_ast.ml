f145e61efb4010a412654cf57f9ae187a3e19ec7
y2k <itwisterlx@gmail.com>
2024-03-18 22:23:54 +0100

    refactoring

--- clj2js/lib/backend_ast.ml
+++ clj2js/lib/backend_ast.ml
@@ -1,12 +1,20 @@
 module A = Angstrom
 open Frontend
 
-let rec cljexp_to_json = function
+let rec cljexp_to_json : _ -> Yojson.Safe.t = function
   | Atom (_, x) when String.starts_with ~prefix:":" x ->
       `String ("\"" ^ String.sub x 1 (String.length x - 1) ^ "\"")
   | Atom (_, x) -> `String x
   | RBList (Atom (_, "module") :: xs) ->
-      cljexp_to_json (xs |> List.rev |> List.hd)
+      let rec loop : cljexp list -> cljexp = function
+        | RBList [ Atom (l, "def"); name; value ] :: tail ->
+            RBList [ Atom (l, "let*"); SBList [ name; value ]; loop tail ]
+        | RBList [ Atom (_, "comment") ] :: tail -> loop tail
+        | [ x ] -> x
+        | [] -> CBList []
+        | x -> fail_node x
+      in
+      xs |> loop |> cljexp_to_json
   | RBList xs -> `List (List.map cljexp_to_json xs)
   | SBList xs -> `List (List.map cljexp_to_json xs)
   | CBList xs -> `List (List.map cljexp_to_json xs)
@@ -15,4 +23,5 @@ let main filename code =
   Frontend.parse_and_simplify 0 filename code
   |> snd
   (* |> fun x -> print_endline (show_cljexp x); x *)
-  |> cljexp_to_json |> Yojson.Safe.to_string
+  |> cljexp_to_json
+  |> Yojson.Safe.to_string
--- clj2js/test/test_ast.ml
+++ clj2js/test/test_ast.ml
@@ -8,5 +8,10 @@ let assert_ code expected =
 let main () =
   assert_ "(Foo.)" {|["new","Foo"]|};
   assert_ "(Foo. a 1)" {|["new","Foo","a","1"]|};
-  assert_ {|(defmacro a [& xs] (list 'b (vec xs))) (a 1 2 3)|} {|["b",["vector","1","2","3"]]|};
+  assert_ {|(defmacro a [& xs] (list 'b (vec xs))) (a 1 2 3)|}
+    {|["b",["vector","1","2","3"]]|};
+  assert_ {|(let [foo (fn [a b] a)](let [bar (fn [a] (foo a a))]))|}
+    {|["let*",["foo",["fn*",["a","b"],"a"]],["let*",["bar",["fn*",["a"],["foo","a","a"]]]]]|};
+  assert_ {|(defn foo [a b] a)(defn bar [a] (foo a a))|}
+    {|["let*",["foo",["fn*",["a","b"],"a"]],["let*",["bar",["fn*",["a"],["foo","a","a"]]],[]]]|};
   ()
