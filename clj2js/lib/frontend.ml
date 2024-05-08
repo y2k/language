15a265075d8b46d9c252e0c087a0e53f7ef151ad
y2k <itwisterlx@gmail.com>
2024-05-08 18:04:17 +0200

    extract ast parser

--- clj2js/lib/frontend.ml
+++ clj2js/lib/frontend.ml
@@ -1,69 +1,4 @@
-open Angstrom
-module A = Angstrom
 open Common
-
-let pnode find_line_and_pos =
-  let pcomment = A.string ";;" *> A.take_while (( <> ) '\n') *> A.char '\n' in
-  let pspace = A.many (A.char ' ' <|> A.char '\n' <|> pcomment) in
-  let patom =
-    A.both A.pos
-      (A.take_while1 (function
-        | ' ' | '\n' | '[' | ']' | '(' | ')' | '{' | '}' -> false
-        | _ -> true))
-    >>| fun (pos, x) ->
-    let line, pos = find_line_and_pos pos in
-    ({ line; pos; symbol = "" }, x)
-  in
-  let patom_ = patom >>| fun (m, a) -> Atom (m, a) in
-  let ptext_ =
-    A.char '"'
-    *> ( A.many1
-           (A.char '\\' *> A.char '"'
-           >>| Fun.const "\\\""
-           <|> (A.satisfy (( <> ) '"') >>| String.make 1))
-       >>| fun x -> String.concat "" x )
-    <* A.char '"'
-  in
-  let pmeta =
-    A.char '^'
-    *> (ptext_
-       <|> A.take_while1 (fun x ->
-               (x >= 'A' && x <= 'z')
-               || x = '.' || x = '(' || x = ')' || x = '-' || x = '>' || x = ':'
-               || x = '?'))
-  in
-  let patom_meta =
-    A.map2 (pmeta <* pspace) patom ~f:(fun m (a, x) ->
-        Atom ({ a with symbol = m }, x))
-  in
-  let ptext = ptext_ >>| fun x -> Atom (unknown_location, "\"" ^ x ^ "\"") in
-  A.many1
-    (A.fix (fun pnode ->
-         pspace
-         *> (ptext <|> patom_meta <|> patom_
-            <|> (A.char '{' *> (A.many (pnode <* pspace) >>| fun xs -> CBList xs)
-                <* A.char '}')
-            <|> (A.char '('
-                 *> (A.many1 (pnode <* pspace) >>| fun xs -> RBList xs)
-                <* A.char ')')
-            <|> (A.char '[' *> (A.many (pnode <* pspace) >>| fun xs -> SBList xs)
-                <* A.char ']'))
-         <* pspace))
-
-let find_line_and_pos str index =
-  let length = String.length str in
-  let rec aux i line pos =
-    if i >= length || i >= index then (line, pos)
-    else if String.get str i = '\n' then aux (i + 1) (line + 1) 1
-    else aux (i + 1) line (pos + 1)
-  in
-  aux 0 1 1
-
-let string_to_sexp code =
-  code
-  |> A.parse_string ~consume:All (pnode (find_line_and_pos code))
-  |> Result.fold ~ok:Fun.id ~error:(fun error ->
-         failwith ("Parse SEXP error: " ^ error))
 
 let desugar_fn_arguments expand_core_macro2 args =
   let rec loop new_args let_args = function
@@ -384,7 +319,8 @@ let parse_and_simplify (prelude_context : context) filename code =
   (* if filename <> "prelude" then
      print_endline "==| DEBUG |==============================================\n"; *)
   let sexp =
-    RBList (Atom (unknown_location, "module") :: string_to_sexp code)
+    RBList
+      (Atom (unknown_location, "module") :: Frontend_parser.string_to_sexp code)
   in
   (* if filename <> "prelude" then print_endline (show_cljexp sexp); *)
   desugar_and_register { prelude_context with filename } sexp |> function
