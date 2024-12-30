8524c8551150d3e0c06648cbad78632ade2c3c1d
y2k <itwisterlx@gmail.com>
2024-12-30 17:20:05 +0100

    clear repository

--- client2/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject app "0.1.0"
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.jsoup/jsoup "1.15.3"]
-                 [http-kit/http-kit "2.6.0"]
-                 [garden/garden "1.3.10"]]
-  :repl-options {:init-ns app.research-framework})
--- compiler/example.clj
+++
@@ -1,8 +0,0 @@
-(module App
-
-  (defn show [env text]
-    (.show 
-      (android.widget.Toast/makeText env.context text 0)))
-
-  (defn main [env]
-    (show env "Hello World")))
