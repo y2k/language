3d4c92d40dcced5a9ff0b85bc549cc5ae9b4644a
y2k <itwisterlx@gmail.com>
2025-01-18 16:04:44 +0100

    [bytecode] fix local function call

--- lib/stage_unwrap_ns.ml
+++ lib/stage_unwrap_ns.ml
@@ -1,7 +1,5 @@
 open Common
 
-type ns_contex = { ns : string }
-
 let try_unwrap_do = function SList (_, [ SAtom (_, "do*"); body ]) -> body | node -> node
 
 let load_file base_path mod_path =
@@ -18,17 +16,19 @@ let load_file base_path mod_path =
   (path, FileReader.read path)
 
 type execute_config = { path : string; code : string }
+type ns_contex = { ns : string; scope : unit StringMap.t }
 
 let invoke (execute_code : execute_config -> sexp) (global_ctx : context) (node : sexp) : sexp =
-  let rec invoke (ctx : ns_contex) = function
-    | SList (m, (SAtom (_, "let*") as l) :: SList (m2, bindings) :: body) ->
-        SList (m, l :: SList (m2, List.map (invoke ctx) bindings) :: List.map (invoke ctx) body)
+  let rec invoke_with_ctx (ctx : ns_contex) node : ns_contex * sexp =
+    let invoke ctx node = invoke_with_ctx ctx node |> snd in
+    match node with
     | SList (m, [ (SAtom (_, "if*") as i); cond; then_; else_ ]) ->
-        SList (m, [ i; invoke ctx cond; invoke ctx then_; invoke ctx else_ ])
-    | SList (m2, [ (SAtom (_, "def*") as def); SAtom (m, name) ]) -> SList (m2, [ def; SAtom (m, ctx.ns ^ "/" ^ name) ])
+        (ctx, SList (m, [ i; invoke ctx cond; invoke ctx then_; invoke ctx else_ ]))
+    | SList (m2, [ (SAtom (_, "def*") as def); SAtom (m, name) ]) ->
+        (ctx, SList (m2, [ def; SAtom (m, ctx.ns ^ "/" ^ name) ]))
     | SList (m2, [ (SAtom (_, "def*") as def); SAtom (m, name); value ]) ->
-        SList (m2, [ def; SAtom (m, ctx.ns ^ "/" ^ name); invoke ctx value ])
-    | SList (m, (SAtom (_, "fn*") as fn_) :: args :: body) -> SList (m, fn_ :: args :: List.map (invoke ctx) body)
+        (ctx, SList (m2, [ def; SAtom (m, ctx.ns ^ "/" ^ name); invoke ctx value ]))
+    | SList (m, (SAtom (_, "fn*") as fn_) :: args :: body) -> (ctx, SList (m, fn_ :: args :: List.map (invoke ctx) body))
     | SList
         ( m,
           (SAtom (_, "do*") as do_)
@@ -53,19 +53,28 @@ let invoke (execute_code : execute_config -> sexp) (global_ctx : context) (node 
                  | n -> failsexp __LOC__ [ n ])
                []
         in
-        let ctx = { ns = ns_name } in
+        let ctx = { ctx with ns = ns_name } in
         let body = List.map (invoke ctx) body in
-        SList (m, do_ :: (inlide_modules @ body)) |> try_unwrap_do
-    | SList (m, (SAtom (_, "do*") as do_) :: body) -> SList (m, do_ :: List.map (invoke ctx) body) |> try_unwrap_do
-    | SList (m, (SAtom (_, "let*") as let_) :: args) -> SList (m, let_ :: List.map (invoke ctx) args)
+        (ctx, SList (m, do_ :: (inlide_modules @ body)) |> try_unwrap_do)
+    | SList (m, (SAtom (_, "do*") as do_) :: body) ->
+        let ctx, body = List.fold_left_map invoke_with_ctx ctx body in
+        (ctx, SList (m, do_ :: body) |> try_unwrap_do)
+    | SList (m, [ (SAtom (_, "let*") as let_); (SAtom (_, name) as n); value ]) ->
+        let ctx = { ctx with scope = StringMap.add name () ctx.scope } in
+        (ctx, SList (m, [ let_; n; invoke ctx value ]))
     | SList (_, SAtom (_, name) :: _) as n when String.ends_with ~suffix:"*" name -> failsexp __LOC__ [ n ]
     (* Function call *)
     | SList (m2, SAtom (m, fn_name) :: args) ->
+        (* prerr_endline @@ "LOG:CALL: " ^ (ctx.scope |> StringMap.bindings |> List.map fst |> String.concat ","); *)
         let args = List.map (invoke ctx) args in
         let prefix =
-          if StringMap.mem fn_name global_ctx.prelude_scope || String.contains fn_name '/' then "" else ctx.ns ^ "/"
+          if
+            StringMap.mem fn_name global_ctx.prelude_scope
+            || String.contains fn_name '/' || StringMap.mem fn_name ctx.scope
+          then ""
+          else ctx.ns ^ "/"
         in
-        SList (m2, SAtom (m, prefix ^ fn_name) :: args)
-    | node -> node
+        (ctx, SList (m2, SAtom (m, prefix ^ fn_name) :: args))
+    | node -> (ctx, node)
   in
-  invoke { ns = "user" } node
+  invoke_with_ctx { ns = "user"; scope = StringMap.empty } node |> snd
