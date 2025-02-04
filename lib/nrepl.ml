f982ede8e49c76fcd59fbb076b7f51c3de985175
y2k <itwisterlx@gmail.com>
2025-02-04 23:23:45 +0100

    add effects

--- lib/nrepl.ml
+++ lib/nrepl.ml
@@ -7,11 +7,10 @@ module EventBus = struct
 
   let make () = { topics = Atomic.make StringMap.empty; id = Atomic.make 1 }
 
-  let rec dispatch (context : t) (msg : msg) : unit =
-    let old_state = Atomic.get context.topics in
-    let new_state = old_state |> StringMap.map (fun xs -> msg :: xs) in
-    if Atomic.compare_and_set context.topics old_state new_state then () else dispatch context msg
+  type _ Effect.t += Dispatch : msg -> unit Effect.t
 
+  let dispatch msg = Effect.perform (Dispatch msg)
+  let sync_time_sec = 0.3
   let gen_id context = Atomic.fetch_and_add context.id 1 |> Int.to_string
 
   let rec update (context : t) (f : _ -> _) =
@@ -19,35 +18,46 @@ module EventBus = struct
     let new_state = f old_state in
     if Atomic.compare_and_set context.topics old_state new_state then old_state else update context f
 
-  let sync_time_sec = 0.3
+  type _ Effect.t += GetMessages : (msg -> 'r option) * float -> 'r list Effect.t
 
-  let listen (context : t) (filter : msg -> 'r option) (listener : 'r list -> unit) : unit =
-    let id = gen_id context in
-    update context (StringMap.add id []) |> ignore;
-    let rec loop () =
-      update context (fun topics -> StringMap.add id [] topics)
-      |> StringMap.find id |> List.filter_map filter |> listener;
-      Unix.sleepf sync_time_sec;
-      loop ()
-    in
-    loop ()
+  let get_messages (filter : msg -> 'r option) ~timeout : 'r list = Effect.perform (GetMessages (filter, timeout))
 
-  let listen_once (context : t) (listener : msg -> 'r option) ~timeout : 'r option =
+  let _get_messages (context : t) (listener : msg -> 'r option) ~timeout : 'r list =
     let id = gen_id context in
     update context (StringMap.add id []) |> ignore;
     let rec loop time_left =
-      if time_left <= 0.0 then None
+      if time_left <= 0.0 then []
       else
         let messages = update context (fun topics -> StringMap.add id [] topics) |> StringMap.find id in
-        match List.find_map listener messages with
-        | Some x -> Some x
-        | None ->
+        match List.filter_map listener messages with
+        | [] ->
             Unix.sleepf sync_time_sec;
             loop (time_left -. sync_time_sec)
+        | xs -> xs
     in
     let result = loop timeout in
     update context (StringMap.remove id) |> ignore;
     result
+
+  let wrap (context : t) f arg =
+    let rec _dispatch (context : t) (msg : msg) : unit =
+      let old_state = Atomic.get context.topics in
+      let new_state = old_state |> StringMap.map (fun xs -> msg :: xs) in
+      if Atomic.compare_and_set context.topics old_state new_state then () else _dispatch context msg
+    in
+    let open Effect.Deep in
+    Effect.Deep.match_with f arg
+      {
+        retc = Fun.id;
+        exnc = (fun e -> Printexc.raise_with_backtrace e (Printexc.get_raw_backtrace ()));
+        effc =
+          (fun (type a) (eff : a Effect.t) ->
+            match eff with
+            | Dispatch msg -> Some (fun (k : (a, _) continuation) -> continue k (_dispatch context msg))
+            | GetMessages (filter, timeout) ->
+                Some (fun (k : (a, _) continuation) -> continue k (_get_messages context filter ~timeout))
+            | _ -> None);
+      }
 end
 
 type msg += CodeCompiled of string | ResultReceived of string
@@ -55,19 +65,20 @@ type msg += CodeCompiled of string | ResultReceived of string
 module ProxyServer = struct
   module S = Tiny_httpd
 
-  let start_proxy_server (context : EventBus.t) port =
+  let start_proxy_server port =
     let server = S.create () ~port in
     S.add_route_handler ~meth:`GET server
       S.Route.(exact_path "read" return)
       (fun _ ->
-        EventBus.listen_once context (function CodeCompiled code -> Some code | _ -> None) ~timeout:300.0
+        EventBus.get_messages (function CodeCompiled code -> Some code | _ -> None) ~timeout:300.0
+        |> Fun.flip List.nth_opt 0
         |> Option.to_result ~none:(503, "Timeout exceeded")
         |> S.Response.make_string);
     S.add_route_handler ~meth:`POST server
       S.Route.(exact_path "write" return)
       (fun req ->
         let result = req.S.Request.body in
-        EventBus.dispatch context (ResultReceived result);
+        EventBus.dispatch (ResultReceived result);
         S.Response.make_string (Ok "OK"));
     Printf.printf "PROXY listening on http://%s:%d\n%!" (S.addr server) (S.port server);
     match S.run server with Ok () -> () | Error e -> raise e
@@ -78,7 +89,7 @@ type msg += NewCodeReceived of string
 module NreplServer = struct
   module B = Bencode
 
-  let handle_client (context : EventBus.t) ic oc =
+  let handle_client ic oc =
     let result = B.decode (`Channel ic) in
     (* print_endline @@ "DECODED: " ^ B.pretty_print result; *)
     let session = "3cea014e-78e1-473e-b486-8f3cab55432a" in
@@ -86,16 +97,12 @@ module NreplServer = struct
     | Some "clone" ->
         B.encode (`Channel oc)
           (B.Dict [ ("id", B.String "1"); ("new-session", B.String session); ("status", B.List [ B.String "done" ]) ])
-    (* { "file": "(ns user) (+ 1 1)",
-           "file-path": "/Users/igor/Projects/language/lib/nrepl.ml",
-           "op": "load-file",
-           "session": "db639f15-5253-4884-bb2c-cf709679270b7" } *)
     | Some "load-file" ->
         let code = B.dict_get result "file" |> Fun.flip Option.bind B.as_string |> Option.get in
-        EventBus.dispatch context (NewCodeReceived code);
+        EventBus.dispatch (NewCodeReceived code);
         let result =
-          EventBus.listen_once context (function ResultReceived code -> Some code | _ -> None) ~timeout:2.0
-          |> Option.value ~default:"<TIMEOUT_ERROR>"
+          EventBus.get_messages (function ResultReceived code -> Some code | _ -> None) ~timeout:2.0
+          |> Fun.flip List.nth_opt 0 |> Option.value ~default:"<TIMEOUT_ERROR>"
         in
         B.encode (`Channel oc)
           (B.Dict [ ("session", B.String session); ("value", B.String result); ("status", B.List [ B.String "done" ]) ])
@@ -106,7 +113,7 @@ module NreplServer = struct
     flush oc;
     print_endline "NREPL client disconnected"
 
-  let start_nrepl_server context port =
+  let start_nrepl_server port =
     let open Unix in
     let addr = ADDR_INET (inet_addr_any, port) in
     let sock = socket PF_INET SOCK_STREAM 0 in
@@ -117,9 +124,7 @@ module NreplServer = struct
       while true do
         let client_sock, _ = accept sock in
         print_endline "NREPL client connected";
-        let ic = in_channel_of_descr client_sock in
-        let oc = out_channel_of_descr client_sock in
-        handle_client context ic oc
+        handle_client (in_channel_of_descr client_sock) (out_channel_of_descr client_sock)
       done
     with e ->
       Printf.eprintf "Error: %s\n" (Printexc.to_string e);
@@ -131,34 +136,38 @@ module Compiler = struct
 
   let compile = Backend_bytecode.main { no_lint = true; virtual_src = "" } false "user.clj" Preludes.bytecode
 
-  let start_compaling (context : EventBus.t) =
-    EventBus.listen context
-      (function NewCodeReceived raw_code -> Some raw_code | _ -> None)
-      (function
-        | code :: _ ->
-            let compiled_code = compile code in
-            EventBus.dispatch context (CodeCompiled compiled_code)
-        | _ -> ())
+  let rec start_compaling () =
+    let codes = EventBus.get_messages (function NewCodeReceived raw_code -> Some raw_code | _ -> None) ~timeout:1.0 in
+    match codes with
+    | code :: _ ->
+        let compiled_code = compile code in
+        EventBus.dispatch (CodeCompiled compiled_code)
+    | _ ->
+        ();
+        start_compaling ()
+end
+
+module Logger = struct
+  let rec listen_logs () =
+    let messages = EventBus.get_messages (fun x -> Some x) ~timeout:1.0 in
+    let log =
+      messages |> List.rev
+      |> List.map (function
+           | CodeCompiled code -> `Assoc [ ("type", `String "CodeCompiled"); ("code", `String code) ]
+           | ResultReceived result -> `Assoc [ ("type", `String "ResultReceived"); ("result", `String result) ]
+           | NewCodeReceived code -> `Assoc [ ("type", `String "NewCodeReceived"); ("code", `String code) ]
+           | _ -> failwith "Unknown command")
+    in
+    if log <> [] then print_endline (Yojson.pretty_to_string (`List log));
+    listen_logs ()
 end
 
 let start () =
   let context = EventBus.make () in
   [
-    Domain.spawn (fun _ -> ProxyServer.start_proxy_server context 8080);
-    Domain.spawn (fun _ -> NreplServer.start_nrepl_server context 8081);
-    Domain.spawn (fun _ -> Compiler.start_compaling context);
-    Domain.spawn (fun _ ->
-        EventBus.listen context
-          (fun x -> Some x)
-          (fun messages ->
-            let log =
-              messages |> List.rev
-              |> List.map (function
-                   | CodeCompiled code -> `Assoc [ ("type", `String "CodeCompiled"); ("code", `String code) ]
-                   | ResultReceived result -> `Assoc [ ("type", `String "ResultReceived"); ("result", `String result) ]
-                   | NewCodeReceived code -> `Assoc [ ("type", `String "NewCodeReceived"); ("code", `String code) ]
-                   | _ -> failwith "Unknown command")
-            in
-            if log <> [] then print_endline (Yojson.pretty_to_string (`List log))));
+    Domain.spawn (fun _ -> EventBus.wrap context NreplServer.start_nrepl_server 8080);
+    Domain.spawn (fun _ -> EventBus.wrap context ProxyServer.start_proxy_server 8081);
+    Domain.spawn (fun _ -> EventBus.wrap context Compiler.start_compaling ());
+    Domain.spawn (fun _ -> EventBus.wrap context Logger.listen_logs ());
   ]
   |> List.iter Domain.join
