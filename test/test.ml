58cd7c91382deef302db408daf32f8515cecf716
y2k <itwisterlx@gmail.com>
2025-09-09 21:11:11 +0200

    change tests structure

--- test/test.ml
+++ test/test.ml
@@ -1,21 +1,21 @@
 let () =
   Alcotest.run ~bail:true "Tests"
     [
-      File_test.tests;
+      Test_file.tests;
       (* *)
       ( "Common (Eval)",
         Test_common.tests_old
-        |> Eval_test.EvalExecution.create_tests `Quick "test/test_common.clj" );
+        |> Test_eval.EvalExecution.create_tests `Quick "test/test_common.clj" );
       ( "Common (Java)",
         Test_common.tests
-        |> Java_test.JavaExecution.create_tests `Quick "test/test_common.clj"
+        |> Test_java.JavaExecution.create_tests `Quick "test/test_common.clj"
              "test_common" );
-      ("Common (JS)", Test_common.tests_old |> Js_test.create_test `Quick);
+      ("Common (JS)", Test_common.tests_old |> Test_js.create_test `Quick);
       (* *)
       ("Sexp", Test_sexp.tests);
-      ("Eval", Eval_test.tests);
-      ("Java", Java_test.tests);
-      ("JS", Js_test.tests);
-      ("Regex", Regex_test.tests);
+      ("Eval", Test_eval.tests);
+      ("Java", Test_java.tests);
+      ("JS", Test_js.tests);
+      ("Regex", Test_regex.tests);
       (* *)
     ]
