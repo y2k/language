6a509457e9be53d3b8ffb8b35babb96803cab2db
y2k <itwisterlx@gmail.com>
2026-01-31 21:52:45 +0100

    Simplify invoke function in frontend_parser.ml
    - Rename invoke_sexp2 to to_sexp with labeled ~replace_col argument
    - Add conv_all helper to reduce List.map repetition
    - Consolidate def*, if*, do* cases using or-pattern
    - Use when guards for cleaner collection handling
    - Remove unreachable catch-all pattern

--- core/frontend_parser.ml
+++ core/frontend_parser.ml
@@ -83,54 +83,49 @@ let string_to_cjexp code =
       failwith ("Parse SEXP error: " ^ error))
 
 let invoke (node : cljexp) : sexp =
-  let rec invoke_sexp2 (replace_col : bool) (node : cljexp) : sexp =
-    let invoke_sexp = invoke_sexp2 replace_col in
-    (* print_endline @@ "== NORM[] == " ^ debug_show_cljexp [ node ]; *)
+  let rec to_sexp ~replace_col node =
+    let conv = to_sexp ~replace_col in
+    let conv_all = List.map conv in
     match node with
-    | RBList (m, [ Atom (lm, "let*"); Atom (nm, name); v ]) ->
-        SList (m, [ SAtom (lm, "let*"); SAtom (nm, name); invoke_sexp v ])
+    (* let* with simple binding: (let* name) or (let* name value) *)
     | RBList (m, [ Atom (lm, "let*"); Atom (nm, name) ]) ->
         SList (m, [ SAtom (lm, "let*"); SAtom (nm, name) ])
+    | RBList (m, [ Atom (lm, "let*"); Atom (nm, name); v ]) ->
+        SList (m, [ SAtom (lm, "let*"); SAtom (nm, name); conv v ])
+    (* let* with bindings vector *)
     | RBList (m, Atom (lm, "let*") :: SBList (bm, bindings) :: body) ->
         SList
           ( m,
-            SAtom (lm, "let*")
-            :: SList (bm, List.map invoke_sexp bindings)
-            :: List.map invoke_sexp body )
+            SAtom (lm, "let*") :: SList (bm, conv_all bindings) :: conv_all body
+          )
+    (* fn* *)
     | RBList (m, (Atom (_, "fn*") as fn) :: SBList (_, args) :: body) ->
         SList
           ( m,
-            invoke_sexp fn
-            :: SList (unknown_location, List.map invoke_sexp args)
-            :: List.map invoke_sexp body )
-    | RBList (m, (Atom (_, "def*") as name) :: xs) ->
-        SList (m, invoke_sexp name :: List.map invoke_sexp xs)
-    | RBList (m, [ (Atom (_, "if*") as i); c; t; e ]) ->
-        SList (m, [ invoke_sexp i; invoke_sexp c; invoke_sexp t; invoke_sexp e ])
+            conv fn :: SList (unknown_location, conv_all args) :: conv_all body
+          )
+    (* quote* - disable collection replacement for quoted content *)
     | RBList (m, [ (Atom (_, "quote*") as q); xs ]) ->
-        SList (m, [ invoke_sexp q; invoke_sexp2 false xs ])
-    | RBList (m, (Atom (_, "do*") as name) :: xs) ->
-        SList (m, invoke_sexp name :: List.map invoke_sexp xs)
-    | RBList (m, name :: xs) -> (
-        match name with
-        | Atom (_, name) when String.ends_with ~suffix:"*" name && name <> "*"
-          ->
-            failnode __LOC__ [ node ]
-        | _ -> SList (m, invoke_sexp name :: List.map invoke_sexp xs))
-    | SBList (m, xs) ->
-        if replace_col then
-          SList
-            (m, SAtom (unknown_location, "vector") :: List.map invoke_sexp xs)
-        else SList (m, List.map invoke_sexp xs)
-    | CBList (m, xs) ->
-        if replace_col then
-          SList
-            (m, SAtom (unknown_location, "hash-map") :: List.map invoke_sexp xs)
-        else SList (m, List.map invoke_sexp xs)
+        SList (m, [ conv q; to_sexp ~replace_col:false xs ])
+    (* Standard forms: def*, if*, do* *)
+    | RBList (m, (Atom (_, ("def*" | "if*" | "do*")) as head) :: xs) ->
+        SList (m, conv head :: conv_all xs)
+    (* Reject unknown starred forms *)
+    | RBList (_, Atom (_, name) :: _)
+      when String.ends_with ~suffix:"*" name && name <> "*" ->
+        failnode __LOC__ [ node ]
+    (* General function application *)
+    | RBList (m, xs) -> SList (m, conv_all xs)
+    (* Collections *)
+    | SBList (m, xs) when replace_col ->
+        SList (m, SAtom (unknown_location, "vector") :: conv_all xs)
+    | CBList (m, xs) when replace_col ->
+        SList (m, SAtom (unknown_location, "hash-map") :: conv_all xs)
+    | SBList (m, xs) | CBList (m, xs) -> SList (m, conv_all xs)
+    (* Atom *)
     | Atom (m, x) -> SAtom (m, x)
-    | n -> failnode __LOC__ [ n ]
   in
-  invoke_sexp2 true node
+  to_sexp ~replace_col:true node
 
 let parse_text code =
   if code = "" then SAtom (meta_empty, "nil")
