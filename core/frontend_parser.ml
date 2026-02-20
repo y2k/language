6d1d97e1822018c2a9e0910bee8f0e7de32e4f84
y2k <itwisterlx@gmail.com>
2026-02-20 22:35:58 +0100

    Rewrite frontend_parser with clearer structure and naming

--- core/frontend_parser.ml
+++ core/frontend_parser.ml
@@ -1,6 +1,9 @@
 open Common
 open Angstrom
-module A = Angstrom
+
+(* ============================================================================
+   Character Predicates
+   ============================================================================ *)
 
 let is_meta_char = function
   | 'A' .. 'z' | '.' | '(' | ')' | '-' | '>' | ':' | '?' | '$' -> true
@@ -10,113 +13,145 @@ let is_atom_char = function
   | ' ' | '\n' | '[' | ']' | '(' | ')' | '{' | '}' -> false
   | _ -> true
 
-let pnode find_line_and_pos =
-  let make_meta pos =
-    let line, pos = find_line_and_pos pos in
+(* ============================================================================
+   Source Position Tracking
+   ============================================================================ *)
+
+let compute_line_and_pos source index =
+  let len = String.length source in
+  let rec loop i line col =
+    if i >= len || i >= index then (line, col)
+    else if source.[i] = '\n' then loop (i + 1) (line + 1) 1
+    else loop (i + 1) line (col + 1)
+  in
+  loop 0 1 1
+
+(* ============================================================================
+   Parser Combinators
+   ============================================================================ *)
+
+let make_parser source =
+  let make_meta offset =
+    let line, pos = compute_line_and_pos source offset in
     { line; pos; symbol = "" }
   in
-  let pcomment =
-    (A.string ";" <|> A.string "#!") *> A.take_while (( <> ) '\n')
-    <* A.many (A.char '\n')
+
+  let comment =
+    (string ";" <|> string "#!") *> take_while (( <> ) '\n') <* many (char '\n')
   in
-  let pspace =
-    A.many (A.char ' ' <|> A.char '\n' <|> (pcomment >>| Fun.const ' '))
+  let whitespace =
+    many (char ' ' <|> char '\n' <|> (comment >>| fun _ -> ' '))
   in
-  let patom =
-    A.both A.pos (A.take_while1 is_atom_char) >>| fun (pos, x) ->
-    (make_meta pos, x)
-  in
-  let ptext_ =
-    let pescape c result = A.char '\\' *> A.char c >>| Fun.const result in
-    A.char '"'
-    *> (A.many1
-          (pescape '"' "\\\"" <|> pescape '\\' "\\\\"
-          <|> (A.satisfy (( <> ) '"') >>| String.make 1))
+
+  let string_content =
+    let escape c replacement = char '\\' *> char c >>| fun _ -> replacement in
+    char '"'
+    *> (many1
+          (escape '"' "\\\"" <|> escape '\\' "\\\\"
+          <|> (satisfy (( <> ) '"') >>| String.make 1))
        >>| String.concat "")
-    <* A.char '"'
+    <* char '"'
   in
-  let pmeta = A.char '^' *> (ptext_ <|> A.take_while1 is_meta_char) in
-  let with_meta parser =
-    A.map2 (pmeta <* pspace) parser ~f:(fun sym (m, x) ->
+
+  let metadata = char '^' *> (string_content <|> take_while1 is_meta_char) in
+
+  let atom =
+    both pos (take_while1 is_atom_char) >>| fun (offset, text) ->
+    (make_meta offset, text)
+  in
+
+  let with_optional_meta parser =
+    map2 (metadata <* whitespace) parser ~f:(fun sym (m, x) ->
         ({ m with symbol = sym }, x))
     <|> parser
   in
-  let patom_meta = with_meta patom >>| fun (m, x) -> Atom (m, x) in
-  let ptext = ptext_ >>| fun x -> Atom (unknown_location, "\"" ^ x ^ "\"") in
-  let make_list start end_ pnode make =
-    let plist =
-      A.char start
-      *> ( A.both (A.many (pnode <* pspace)) A.pos >>| fun (xs, pos) ->
-           (make_meta pos, xs) )
-      <* A.char end_
-    in
-    with_meta plist >>| fun (m, xs) -> make m xs
-  in
-  A.many1
-    (A.fix (fun pnode ->
-         pspace
-         *> (ptext <|> patom_meta
-            <|> make_list '(' ')' pnode (fun m v -> RBList (m, v))
-            <|> make_list '{' '}' pnode (fun m v -> CBList (m, v))
-            <|> make_list '[' ']' pnode (fun m v -> SBList (m, v)))
-         <* pspace))
 
-let find_line_and_pos str index =
-  let length = String.length str in
-  let rec aux i line pos =
-    if i >= length || i >= index then (line, pos)
-    else if String.get str i = '\n' then aux (i + 1) (line + 1) 1
-    else aux (i + 1) line (pos + 1)
+  let atom_with_meta = with_optional_meta atom >>| fun (m, x) -> Atom (m, x) in
+
+  let string_literal =
+    string_content >>| fun x -> Atom (unknown_location, "\"" ^ x ^ "\"")
   in
-  aux 0 1 1
+
+  let make_bracketed_list start_char end_char make_node node_parser =
+    let inner =
+      char start_char
+      *> ( both (many (node_parser <* whitespace)) pos >>| fun (xs, offset) ->
+           (make_meta offset, xs) )
+      <* char end_char
+    in
+    with_optional_meta inner >>| fun (m, xs) -> make_node m xs
+  in
+
+  many1
+    (fix (fun node_parser ->
+         whitespace
+         *> (string_literal <|> atom_with_meta
+            <|> make_bracketed_list '(' ')'
+                  (fun m xs -> RBList (m, xs))
+                  node_parser
+            <|> make_bracketed_list '{' '}'
+                  (fun m xs -> CBList (m, xs))
+                  node_parser
+            <|> make_bracketed_list '[' ']'
+                  (fun m xs -> SBList (m, xs))
+                  node_parser)
+         <* whitespace))
+
+(* ============================================================================
+   Parse Entry Point
+   ============================================================================ *)
 
 let string_to_cljexp code =
-  code
-  |> A.parse_string ~consume:All (pnode (find_line_and_pos code))
-  |> Result.fold ~ok:Fun.id ~error:(fun e ->
-      failwith ("Parse SEXP error: " ^ e))
+  match parse_string ~consume:All (make_parser code) code with
+  | Ok result -> result
+  | Error msg -> failwith ("Parse SEXP error: " ^ msg)
+
+(* ============================================================================
+   cljexp -> sexp Transformation
+   ============================================================================ *)
 
 let invoke (node : cljexp) : sexp =
-  let rec to_sexp ~replace_col node =
-    let conv = to_sexp ~replace_col in
-    let conv_all = List.map conv in
+  let rec transform ~(convert_collections : bool) (node : cljexp) : sexp =
+    let go n = transform ~convert_collections:true n in
+    let go_all ns = List.map go ns in
     match node with
+    | Atom (m, x) -> SAtom (m, x)
     | RBList (m, [ Atom (lm, "let*"); Atom (nm, name) ]) ->
         SList (m, [ SAtom (lm, "let*"); SAtom (nm, name) ])
-    | RBList (m, [ Atom (lm, "let*"); Atom (nm, name); v ]) ->
-        SList (m, [ SAtom (lm, "let*"); SAtom (nm, name); conv v ])
+    | RBList (m, [ Atom (lm, "let*"); Atom (nm, name); value ]) ->
+        SList (m, [ SAtom (lm, "let*"); SAtom (nm, name); go value ])
     | RBList (m, Atom (lm, "let*") :: SBList (bm, bindings) :: body) ->
         SList
-          ( m,
-            SAtom (lm, "let*") :: SList (bm, conv_all bindings) :: conv_all body
-          )
+          (m, SAtom (lm, "let*") :: SList (bm, go_all bindings) :: go_all body)
     | RBList (m, (Atom (_, "fn*") as fn) :: SBList (_, args) :: body) ->
-        SList
-          ( m,
-            conv fn :: SList (unknown_location, conv_all args) :: conv_all body
-          )
+        SList (m, go fn :: SList (unknown_location, go_all args) :: go_all body)
     | RBList (m, [ (Atom (_, "quote*") as q); xs ]) ->
-        SList (m, [ conv q; to_sexp ~replace_col:false xs ])
+        SList (m, [ go q; transform ~convert_collections:false xs ])
     | RBList (m, (Atom (_, ("def*" | "if*" | "do*")) as head) :: xs) ->
-        SList (m, conv head :: conv_all xs)
+        SList (m, go head :: go_all xs)
     | RBList (_, Atom (_, name) :: _)
       when String.ends_with ~suffix:"*" name && name <> "*" ->
         failnode __LOC__ [ node ]
-    | RBList (m, xs) -> SList (m, conv_all xs)
-    | SBList (m, xs) when replace_col ->
-        SList (m, SAtom (unknown_location, "vector") :: conv_all xs)
-    | CBList (m, xs) when replace_col ->
-        SList (m, SAtom (unknown_location, "hash-map") :: conv_all xs)
-    | SBList (m, xs) | CBList (m, xs) -> SList (m, conv_all xs)
-    | Atom (m, x) -> SAtom (m, x)
+    | RBList (m, xs) -> SList (m, go_all xs)
+    | SBList (m, xs) when convert_collections ->
+        SList (m, SAtom (unknown_location, "vector") :: go_all xs)
+    | CBList (m, xs) when convert_collections ->
+        SList (m, SAtom (unknown_location, "hash-map") :: go_all xs)
+    | SBList (m, xs) | CBList (m, xs) -> SList (m, go_all xs)
   in
-  to_sexp ~replace_col:true node
+  transform ~convert_collections:true node
+
+(* ============================================================================
+   Main API
+   ============================================================================ *)
 
 let parse_text code =
   if code = "" then SAtom (meta_empty, "nil")
   else
-    string_to_cljexp code
-    |> ( function
-    | [ x ] -> x
-    | xs -> RBList (meta_empty, Atom (meta_empty, "do") :: xs) )
-    |> invoke
+    let nodes = string_to_cljexp code in
+    let cljexp =
+      match nodes with
+      | [ single ] -> single
+      | multiple -> RBList (meta_empty, Atom (meta_empty, "do") :: multiple)
+    in
+    invoke cljexp
